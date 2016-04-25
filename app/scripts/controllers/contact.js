772400744016c17e3e2337f22ee89200ac7072b7
Travis Salad <travissalad@gmail.com>
2016-04-24 21:45:59 -0700

    added new contact view and added content

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTestApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the angularTestApp
+ */
+angular.module('angularTestApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
