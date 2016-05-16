490bc6d4bb45ee17976f2671481501d0d1be0a77
Travis Salad <travissalad@gmail.com>
2016-05-16 18:09:39 -0400

    added marker to map, recenter to map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,34 +13,50 @@
      angular.extend($scope, {
        map: {
          center: {
-           latitude: 40.1451,
-           longitude: -99.6680
+           latitude: 47.6062095,
+           longitude: -122.3320708
          },
-         zoom: 8
+         zoom: 9
        },
        searchbox: {
          template:'searchbox.tpl.html',
          events:{
-              places_changed: function(searchBox) {
-              var location = searchBox.getPlaces();
-              $scope.placesID = location[0].place_id;
-           }
+           places_changed: function(searchBox) {
+           var location = searchBox.getPlaces();
+           $scope.placesID = location[0].place_id;
+
+
+
+           $scope.addPlaces = function(){
+             $scope.placesFound = current.query({
+                placeID: $scope.placesID
+              });
+              $scope.map = {
+                "center": {
+                  "latitude": location[0].geometry.location.lat(),
+                  "longitude": location[0].geometry.location.lng()
+                 },
+                "zoom": 9
+              };
+            };
+
+
+            $scope.findPlaces = function(){
+              $scope.map = {
+                "center": {
+                  "latitude": location[0].geometry.location.lat(),
+                  "longitude": location[0].geometry.location.lng()
+                 },
+                "zoom": 9
+              };
+            };
+
+
+          }
          }
-       },
+      },
        options: {
          scrollwheel: false
        }
-     });
-
-     $scope.placesFound = current.query();
-
-     $scope.findPlaces = function(){
-       $scope.placesFound = current.query({
-         placeID: $scope.placesID
-      });
-    };
-
-     GoogleMapApi.then(function(maps) {
-       maps.visualRefresh = true;
      });
  }]);
--- app/scripts/services/current.js
+++ app/scripts/services/current.js
@@ -9,15 +9,15 @@
  */
 
 angular.module('angularTestApp')
-  .factory('current', function($resource){
+.factory('current', function($resource){
 
-    return $resource('https://crossorigin.me/https://maps.googleapis.com/maps/api/place/details/json?placeid=:placeID&key=AIzaSyANULjox-0qcLdFkdlsYBoj4iMlOUiS5_g', {}, {
-        query: {
-          method: 'GET',
-          params: {
-            placeID: 'ChIJyU1YTzwC54kRj4V6zn6M68s',
-          },
-        isArray: false
+  return $resource('https://crossorigin.me/https://maps.googleapis.com/maps/api/place/details/json?placeid=:placeID&key=AIzaSyANULjox-0qcLdFkdlsYBoj4iMlOUiS5_g', {}, {
+      query: {
+        method: 'GET',
+        params: {
+          placeID: 'ChIJyU1YTzwC54kRj4V6zn6M68s',
+        },
+      isArray: false
       }
     });
   });
