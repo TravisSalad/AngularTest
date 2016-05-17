480aa1ec3ccad698978e9adcaed430cc6c408df7
Travis Salad <travissalad@gmail.com>
2016-05-16 22:02:47 -0400

    added local storage, TODO extract local storage and add to ng-repeat li

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 
  angular.module('angularTestApp')
- .controller('MainCtrl', ['$scope', '$log', 'uiGmapGoogleMapApi', 'current', function ($scope, $log, GoogleMapApi, current) {
+ .controller('MainCtrl', ['$scope', '$log', 'uiGmapGoogleMapApi', 'current', '$localStorage', function ($scope, $log, GoogleMapApi, current, $localStorage) {
      angular.extend($scope, {
        map: {
          center: {
@@ -25,47 +25,53 @@
        searchbox: {
          template:'searchbox.tpl.html',
          events:{
-           places_changed: function(searchBox) {
-           var location = searchBox.getPlaces();
-           $scope.placesID = location[0].place_id;
+             places_changed: function(searchBox) {
+             var location = searchBox.getPlaces();
+             $scope.placesID = location[0].place_id;
 
 
 
-           $scope.addPlaces = function(){
-             $scope.placesFound = current.query({
-                placeID: $scope.placesID
-              });
-              $scope.map = {
-                "center": {
-                  "latitude": location[0].geometry.location.lat(),
-                  "longitude": location[0].geometry.location.lng()
-                 }
+             $scope.addPlaces = function(){
+               $scope.placesFound = current.query({
+                  placeID: $scope.placesID
+                });
+
+                $scope.$storage = $localStorage;
+                $localStorage.name = location[0].name;
+                $localStorage.lat = location[0].geometry.location.lat();
+                $localStorage.lng = location[0].geometry.location.lng();
+
+                $scope.map = {
+                  center: {
+                    latitude: location[0].geometry.location.lat(),
+                    longitude: location[0].geometry.location.lng()
+                   }
+                };
+                $scope.coords = {
+                  latitude: location[0].geometry.location.lat(),
+                  longitude: location[0].geometry.location.lng()
+                };
               };
-              $scope.coords = {
-                "latitude": location[0].geometry.location.lat(),
-                "longitude": location[0].geometry.location.lng()
+
+
+              $scope.findPlaces = function(){
+                $scope.map = {
+                  center: {
+                    latitude: location[0].geometry.location.lat(),
+                    longitude: location[0].geometry.location.lng()
+                   },
+                  zoom: 12
+                };
+                $scope.coords = {
+                  latitude: location[0].geometry.location.lat(),
+                  longitude: location[0].geometry.location.lng()
+                };
               };
-            };
 
 
-            $scope.findPlaces = function(){
-              $scope.map = {
-                "center": {
-                  "latitude": location[0].geometry.location.lat(),
-                  "longitude": location[0].geometry.location.lng()
-                 },
-                "zoom": 12
-              };
-              $scope.coords = {
-                "latitude": location[0].geometry.location.lat(),
-                "longitude": location[0].geometry.location.lng()
-              };
-            };
-
-
+            }
           }
-        }
-       },
+        },
        options: {
          scrollwheel: false
        }
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/lodash/lodash.js',
       'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/ngPlacesAutocomplete/ngPlacesAutocomplete.min.js',
+      'bower_components/ngstorage/ngStorage.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
